3ba0f8bec22359761c224d91886939575acfb099
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-03-21 09:50:08 +0000

    Generate test using github for create dir, using copy-file as template

---
+++ code/blog-generation/create-dir.test.ts
@@ -0,0 +1,28 @@
+import assert from "node:assert";
+import { describe, test, mock, beforeEach, afterEach } from "node:test";
+
+describe("Test create-dir.ts", async () => {
+  const mkdirSyncMock = mock.fn();
+
+  beforeEach(async () => {
+    const fsExports = await import("fs").then(({ default: _, ...rest }) => ({
+      ...rest,
+      mkdirSync: mkdirSyncMock,
+    }));
+    mock.module("fs", {
+      namedExports: fsExports,
+    });
+  });
+
+  afterEach(() => {
+    mock.restoreAll();
+  });
+
+  test("Ensure fs.mkdirSync is called with correct arguments", async () => {
+    const testee = await import("./create-dir.ts");
+    testee.default("./test-dir");
+    assert.strictEqual(mkdirSyncMock.mock.callCount(), 1);
+    assert.strictEqual(mkdirSyncMock.mock.calls[0].arguments[0], "./test-dir");
+    assert.deepStrictEqual(mkdirSyncMock.mock.calls[0].arguments[1], { recursive: true });
+  });
+});
